46654c3026a6db828e88a5c59fef7aebac7823ae
Nurckye <radu.nitescu35@gmail.com>
2020-05-16 21:21:08 +0300

    Added build

---
+++ build/precache-manifest.77161dc523f6ad03640721de11af0b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbc9bf163a76a26204af5fb93304296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb6ade841702fbcb802",
+    "url": "/static/css/main.a7fedea8.chunk.css"
+  },
+  {
+    "revision": "859611a44047f7596619",
+    "url": "/static/js/2.947c4e1f.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.947c4e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb6ade841702fbcb802",
+    "url": "/static/js/main.91e931bf.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
